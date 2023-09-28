0e832720ec6cfedd5e5c98bf5837482f7dfbb948
Gilgamesh <Gilgamesh>
2023-09-28 20:42:09 +0200

    Randomisation lg heritage maire

---
+++ src/main/java/org/apache/maven/archetypes/CapitalSpéLoupGarous/Personnages/Fonction.java
@@ -0,0 +1,40 @@
+package org.apache.maven.archetypes.CapitalSpéLoupGarous.Personnages;
+
+import org.apache.maven.archetypes.CapitalSpéLoupGarous.Village;
+
+public abstract class Fonction {
+	private Personnage personnage;
+	private Village village;
+	private boolean estMaire;
+
+	public Fonction(boolean estMaire) {
+		this.estMaire = estMaire;
+	}
+
+	public Personnage getPersonnage() {
+		return personnage;
+	}
+
+	public void setPersonnage(Personnage personnage) {
+		this.personnage = personnage;
+	}
+
+	public Village getVillage() {
+		return village;
+	}
+
+	public void setVillage(Village village) {
+		this.village = village;
+	}
+	
+	public boolean estMaire() {
+		return this.estMaire;
+	}
+	
+	public void reset() {
+		this.personnage = null;
+		this.village = null;
+	}
+	
+
+}
---
+++ src/main/java/org/apache/maven/archetypes/CapitalSpéLoupGarous/Personnages/Maire.java
@@ -0,0 +1,43 @@
+package org.apache.maven.archetypes.CapitalSpéLoupGarous.Personnages;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import org.apache.maven.archetypes.CapitalSpéLoupGarous.Logger;
+import org.apache.maven.archetypes.CapitalSpéLoupGarous.Village;
+
+public class Maire extends Fonction {
+
+	public Maire() {
+		super(true);
+	}
+	
+	public void entrerEnFonction(Personnage personnage, Village village) {
+		super.setPersonnage(personnage);
+		super.setVillage(village);
+		village.setMaire(personnage);
+		personnage.setFonction(this);
+		personnage.incrementerNbVote(1);
+		Logger.log(personnage + " a été élue maire du village");
+	}
+	
+	public void election() {
+		int nb;
+		if(this.getPersonnage() != null) {
+			this.getPersonnage().setAlliés(new ArrayList<Personnage>(this.getPersonnage().getAlliés().stream().filter(x->x.estEnvie()).collect(Collectors.toList())));//maj alliés
+			if(this.getPersonnage().getAlliés().size() > 1 && this.getPersonnage().estUnVillageois()) {// si au moins un alliés et villageois
+				nb = (int) (Math.random() * ( this.getPersonnage().getAlliés().size()    - 0 ));
+				entrerEnFonction(this.getPersonnage().getAlliés().get(nb), this.getVillage());
+			}
+			else {// sinon full random
+				nb = (int) (Math.random() * ( this.getVillage().getNbPersonnage()    - 0 ));
+				entrerEnFonction(this.getVillage().getHabitantsEnVie().get(nb), this.getVillage());
+			}
+		}
+		else {// 1er maire
+			nb = (int) (Math.random() * ( this.getVillage().getNbPersonnage()    - 0 ));
+			entrerEnFonction(this.getVillage().getHabitantsEnVie().get(nb), this.getVillage());
+		}
+	}
+
+}
