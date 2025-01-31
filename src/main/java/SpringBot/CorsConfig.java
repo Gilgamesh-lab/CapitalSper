1992c70676aaa79fa9cccc87c2be6c7a3bc0873a
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2025-01-31 19:23:30 +0100

    Autoriser Cors

--- src/main/java/SpringBot/CorsConfig.java
+++ src/main/java/SpringBot/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://capitalsper.web.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
